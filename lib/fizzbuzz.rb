4e55d1bb17cf7f6afb0bdb9f7a545ef9e15c7678
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-01 16:04:27 +0100

    Updated ruby code logic and rspec test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-  'fizz'
+  if number % 3 == 0 && number % 5 == 0
+    puts "fizzbuzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else 
+    number.to_s
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,20 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end 
+  (1..100).each do |n|
+    it "returns the correct output for #{n}" do
+      expected_output =
+      case 
+      when n % 3 == 0 && n % 5 == 0
+        'fizzbuzz'
+      when n % 3 == 0
+        'fizz'
+      when n % 5 == 0
+        'buzz'
+      else 
+        n.to_s
+      end
+
+      expect(fizzbuzz(n)).to eq(expected_output)
+    end
+  end
 end
